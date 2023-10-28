f176c08c8d04caa3fd2fc08ea906b454ecffaa78
Kalu Chinonso <tripletens.kc@gmail.com>
2023-10-28 15:08:28 +0100

    add the keys migration

--- app/Livewire/Pages/TestKey.php
+++ app/Livewire/Pages/TestKey.php
@@ -1,11 +1,31 @@
 <?php
 
 namespace App\Livewire\Pages;
-
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class TestKey extends Component
 {
+    public $inputtype = "password";
+
+    public function toggle_type(){
+        $this->inputtype = ($this->inputtype === 'password') ? 'text' : 'password';
+    }
+
+
+    // fetch user test key
+
+
+    // create new test key
+
+    public function create_test_key(){
+        $key = Str::random(40);
+
+        $user_id 
+    }
+
+    // copy test key
+
     public function render()
     {
         return view('livewire.pages.test-key');
---
+++ database/migrations/2023_10_28_140121_create_keys_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // ['user_id','value','status']
+        Schema::create('keys', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->nullable();
+            $table->string('value')->nullable();
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('keys');
+    }
+};
